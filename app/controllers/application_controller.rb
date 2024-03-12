88978d1a66a19d568558a433b6f9ae1aa4e7ab14
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-12 15:37:11 -0400

    Adicionando telefone e nome no devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  #ApplicationController, que é a classe pai de todos os controladores na sua aplicação Rails:
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :phone])
+  end
+
+  #Isso é usado para garantir que apenas parâmetros específicos sejam aceitos para evitar ataques de mass assignment.
+  #Os parâmetros permitidos definidos neste método incluem :name, :email, :password, :password_confirmation e :phone,  
+  #o que significa que esses campos podem ser enviados no formulário de inscrição do Devise.
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_10_184142) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_151940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_10_184142) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.string "phone"
+    t.string "name", null: false
+    t.string "phone", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
