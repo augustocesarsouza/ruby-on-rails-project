166aa4a544f329bbf292a26b6b9e614c3d4846b6
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-12 16:18:36 -0400

    criando migrate para adicionar role user

--- db/migrate/20240312193820_add_column_role_to_user.rb
+++ db/migrate/20240312193820_add_column_role_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnRoleToUser < ActiveRecord::Migration[7.1]
   def change
-    create_enum :role_user, ['user', 'admin']
+    create_enum :role_user, ['user', 'admin'] # esses enum é do postgresql
 
     change_table :users do |t|
       t.enum :role, enum_type: 'role_user', default: 'user', null: false
