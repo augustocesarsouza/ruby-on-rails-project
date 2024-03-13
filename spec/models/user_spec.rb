702bd52de3d2eb3fdbb0ec7ac5aaa49fd62c2355
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-13 11:30:54 -0400

    criando estilo para o usuario entrar sair se registrar

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe User, type: :model do
       # expect(user.valid?).to eq(true)
     end
 
-    it "Verifica se o campo nome não está presente" do 
+    it 'Verifica se o campo nome não está presente' do 
       # Arrange, Act, Assert
       user = build(:user, name: nil) # não precisa colocar FactoryBot.build, só build ele consegue dectar porque já foi vinculado com RSpec spec/support/factory_bot.rb
 
       expect(user.valid?).to be_falsey # <- esse jeito é mais usado em espresas be_truthy/be_falsey
     end
-
+    
     it "Verifica se o campo phone não está presente" do 
       # Arrange, Act, Assert
       user = build(:user, phone: nil) 
