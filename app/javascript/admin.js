db5839b4f01fde1d69347a00009e202c5fdfe6dd
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-13 12:04:54 -0400

    criando admin style e javascript, vinculando no packge.json script e colocando no procifile

---
+++ app/javascript/admin.js
@@ -0,0 +1,4 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "controllers"
+import "./sbadmin/sbadmin.min.js" 
+import * as bootstrap from "bootstrap"
