2811c1ae74c6c77ad2a17847bed080d40db88e05
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-14 11:16:47 -0400

    adicionando comments

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class BaseController < ApplicationController
     layout 'admin' # trocando porque por padrão ele usar leyaout application, com esse "layout" conseguimos configurar nosso layout
-    before_action :authenticate_user! # antes de chamar a action, ele passa por essa primeira para ver se o usiaro é admin
+    before_action :authenticate_user! # ISSO VE SE O USUARIO ESTÁ LAGADO! antes de chamar a action, ele passa por essa primeira para ver se o usiaro é admin,
     before_action :only_admin!
 
     private def only_admin!
