a90dc7797d92824a4e92203b17dcd49364cd429f
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-16 19:12:47 -0400

    arrumando select

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @products = Product.select(:id, :name, :price).with_attached_image # precisa do id para fazer "attached" da imagem
+    # @products = Product.all
   end
 end
 
