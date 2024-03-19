7b3d4e89ae957886ca6334f5628801b229ef096e
augustocesarsouza <augustocesarsantana90@gmail.com>
2024-03-19 10:12:33 -0400

    adicionando flash

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,9 +7,11 @@ class SettingsController < ApplicationController
   def update_user
     if current_user.update(user_params)
       redirect_to settings_path, notice: "user was successfully updated."
+      # redirect_to - ele de fato te redireciona, ai ele faz um 'referesh' na pagina toda
     else
       render :index # rederiza a view novamente "index" porque o objeto "current_user" vai estar atualiado com as mensagem de erro e o "simple_form_for" vao preencher com error
-    end
+      # 'render' nao da refreshing na pagina, leva os dados 'current_user'
+    end 
   end
 
   private
